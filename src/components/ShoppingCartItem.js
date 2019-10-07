415eed3f56128dac112035241b992251bd66b532
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-07 15:56:00 -0700

    Added contexts folder and useContext

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from "./components/CartContext";
 
 const Item = props => {
+	const { removeItem } = useContext(CartContext);
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
-
-
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
